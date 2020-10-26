c6891c481ba921d76bb4b1594805f3589860268d
unknown <andel@nbaschulz.optimal-systems.de>
2020-10-26 16:18:47 +0100

    next try

--- functions/shared.js
+++ functions/shared.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 // const getAccessToken = require('./utils/auth.js').getAccessToken;
-const getAccessToken = require('utils').getAccessToken;
-const API = require('utils').API;
+const getAccessToken = require('./utils/index.js').getAccessToken;
+const API = require('./utils/index.js').API;
 
 exports.handler = function (event, context, callback) {
 
